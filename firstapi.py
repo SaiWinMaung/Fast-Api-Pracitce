aacfdd216eead5693f394635c6e017156ab36be4
SaiWinMaung <saiwinmaung.mc@gmail.com>
2025-01-30 16:06:45 +0630

    auth for CRUD

--- firstapi.py
+++ firstapi.py
@@ -1,51 +1,71 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/", description="This is our first route")
-async def root():
-    return {"message": "hello world"}
+students = {
+    1: {
+        'name' : 'Sai Win Maung',
+        'age' : 12,
+        'major' : 'Mechatronics'
+    }
+}
+
+class Student(BaseModel):
+    name : str
+    age : int
+    major : str
+
+class UpdateStudent(BaseModel):
+    name : Optional[str] = None
+    age : Optional [int] = None
+    major : Optional[str] = None
+     
 
 
-@app.post("/")
-async def post():
-    return {"message": "Hello from the post route"}
+@app.get('/get_student/{student_id}')
+async def get_student(student_id :int):
+    return students[student_id]
 
 
-@app.put("/")
-async def put():
-    return {"message": "Hello from the put route"}
+@app.get('/get_student_byname/{student_id}')
+async def get_byname(name :str, student_id :int):
+    for stuent in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return {'Data' : 'Not Found'}
 
 
-@app.delete("/")
-async def delete():
-    return {"message": "Hello from the delete route"}
+@app.post('/create_student/{student_id}')
+async def create_student(student_id : int, student : Student):
+    if student_id in students:
+        return { 'Error' : 'Student has been exist'}
+    
+    students[student_id] = student
+    return students[student_id]
 
 
-@app.get("/items")
-async def list_item():
-    return {"message": "List Item route"}
+@app.put('/update_student/{student_id}')
+async def update_student(student_id : int, student : UpdateStudent):
+    if student_id not in  students:
+        return { 'Error' : 'Student does not exist'}
+    
+    if student.name != None:
+        students[student_id].name = student.name
 
+    if student.age != None:
+        students[student_id].age = student.age
+    if student.major != None:
+        students[student_id].major = student.major
+    
+    return students[student_id]
 
-@app.get("/item/{item_id}")
-async def get_item(item_id, isset=False):
-    if not isset:
-        item_id = {"message": f"Item id is {item_id}"}
-    return item_id
-
-
-@app.get("/user/{user_id}/item/{item_id}")
-async def get_user(
-    item_id: int, user_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {
-                "description": "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-            }
-        )
-    return item
+@app.delete('/delete_student/{student_id}')
+async def delete_student(student_id :int):
+    if student_id not in students:
+        return {'Error' : 'Student does not exist'}
+    
+    del students[student_id]
+    return { "Message" : 'Student Delete Successful'}
